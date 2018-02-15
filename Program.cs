07d0e66f86d913dc4a754b0a4680d50799c9870d
Vamshi Basupalli <vamshi@cs.wisc.edu>
2018-02-15 16:02:59 -0600

    Initial version

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Build.Evaluation;
+//using Microsoft.DotNet.Cli.Sln;
+
+namespace CsProjReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csproj_file = args[0];
+            Console.WriteLine(csproj_file);
+            var proj = new Project(csproj_file);
+            Console.WriteLine(proj.Properties.Count);
+
+            foreach (ProjectProperty item in proj.Properties)
+            {
+                Console.WriteLine("{0} = {1}", item.Name, item.EvaluatedValue);
+            }
+        }
+    }
+}
